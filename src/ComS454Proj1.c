e06b21d6bc569b80b7fdf34bab1112fa825157e7
Robert Clabough <rmcn96@gmail.com>
2015-02-05 11:10:22 -0600

    Initial Commit

---
+++ src/ComS454Proj1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : ComS454Proj1.c
+ Author      : Robert Clabough
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
